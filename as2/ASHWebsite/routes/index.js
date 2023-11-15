2468cc902870d4058eed16a3119c801fa4515401
ashscripting <arshia.mortazavinezhad@ontariotechu.net>
2023-11-15 11:51:35 -0500

    assignment 3 start

--- as2/ASHWebsite/routes/index.js
+++ as2/ASHWebsite/routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,20 +15,6 @@ router.get('/home', function(req, res, next) {
     title: 'Home'
   });
 });
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', {
-    title: 'About'
-  });
-});
-
-/* GET projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('projects', {
-    title: 'Projects'
-  });
-});
-
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', {
@@ -40,5 +22,15 @@ router.get('/contact', function(req, res, next) {
   });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    title: 'Login'
+  });
+});
 
+router.get('/sign-up', function(req, res, next) {
+  res.render('signup', {
+    title: 'Sign Up'
+  });
+});
 module.exports = router;
